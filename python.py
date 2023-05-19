3a6fff1449918bd1fe6bd2ed6ac19d2c4c7909ec
giuliasb296 <123772160+giuliasb296@users.noreply.github.com>
2023-05-19 11:49:38 -0300

    Aula 1

---
+++ python.py
@@ -0,0 +1,29 @@
+print ("Mensagem que eu quero", "TESTE", sep="")
+
+print ('Mensagem 2')
+
+print ('Fulano é meu "amigo"')
+
+print ('Fulano \n é meu \"amigo\"')
+
+print(1+1)
+
+print("1 x 1 =",1*1)
+
+print("10³ =", (10**3))
+
+print(pow(10,3))
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
+
+D1 = 5.5
+D2 = "5"
+print(int(D1 + int(D2)))
+print(round(D1 + int(D2)))
